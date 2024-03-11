0772ce3b31ec1207d481aae880f3bbf013a041c9
Gygel <florinpetrescu70@gmail.com>
2024-03-11 13:50:29 +0100

    Updated buttons + Decrement btn
    Made buttons bigger for mobile and added a decrement button for each category

--- script.js
+++ script.js
@@ -17,6 +17,17 @@ function increment(elemID){
     console.log(elemID + ": " + dict[elemID])
 }
 
+function decrement(elemID){
+    if(dict[elemID] > 0){
+        dict[elemID] -= 1
+        document.getElementById(elemID).innerText = dict[elemID]
+        console.log(elemID + ": " + dict[elemID])
+    }
+    else{
+        console.log("Cannot go below 0")
+    }
+}
+
 
 function setToZero(elemID){
     dict[elemID] = 0
@@ -28,19 +39,24 @@ let segTBox = document.getElementById("segment-el")
 let sNameTBox = document.getElementById("street-name-el")
 
 function save(){
-    let table = document.getElementById("tbody-el")
-
-    table.innerHTML += "<tr><td rowspan ='4'>"+ segTBox.value +"</td> <td rowspan ='4'>"+sNameTBox.value+"</td> </tr>"
-    table.innerHTML +="<tr><th>Well parked</th>  <td>"+dict['NORM_WP-counter-el']+"</td> <td>"+dict['ELEC_WP-counter-el']+"</td> <td>"+(dict['NORM_WP-counter-el']+dict['ELEC_WP-counter-el'])+"</td> </tr>"
-    table.innerHTML +="<tr><th>Half parked</th>  <td>"+dict['NORM_HP-counter-el']+"</td> <td>"+dict['ELEC_HP-counter-el']+"</td> <td>"+(dict['NORM_HP-counter-el']+dict['ELEC_HP-counter-el'])+"</td> </tr>"
-    table.innerHTML +="<tr><th>Badly parked</th>  <td>"+dict['NORM_BP-counter-el']+"</td> <td>"+dict['ELEC_BP-counter-el']+"</td> <td>"+(dict['NORM_BP-counter-el']+dict['ELEC_BP-counter-el'])+"</td> </tr>"
-    
-    segTBox.value = ""
-    sNameTBox.value = ""
-
-    for(var [key,value] of Object.entries(dict)){
-        setToZero(key)
+    if(segTBox.value == "" || sNameTBox.value == ""){
+        alert("Input the Segment and Street Name!")
     }
+    else{
+        let table = document.getElementById("tbody-el")
 
-    console.log("row inserted")
+        table.innerHTML += "<tr><td rowspan ='4'>"+ segTBox.value +"</td> <td rowspan ='4'>"+sNameTBox.value+"</td> </tr>"
+        table.innerHTML +="<tr><th>Well parked</th>  <td>"+dict['NORM_WP-counter-el']+"</td> <td>"+dict['ELEC_WP-counter-el']+"</td> <td>"+(dict['NORM_WP-counter-el']+dict['ELEC_WP-counter-el'])+"</td> </tr>"
+        table.innerHTML +="<tr><th>Half parked</th>  <td>"+dict['NORM_HP-counter-el']+"</td> <td>"+dict['ELEC_HP-counter-el']+"</td> <td>"+(dict['NORM_HP-counter-el']+dict['ELEC_HP-counter-el'])+"</td> </tr>"
+        table.innerHTML +="<tr><th>Badly parked</th>  <td>"+dict['NORM_BP-counter-el']+"</td> <td>"+dict['ELEC_BP-counter-el']+"</td> <td>"+(dict['NORM_BP-counter-el']+dict['ELEC_BP-counter-el'])+"</td> </tr>"
+        
+        segTBox.value = ""
+        sNameTBox.value = ""
+
+        for(var [key,value] of Object.entries(dict)){
+            setToZero(key)
+        }
+
+        console.log("row inserted")
+    }
 }
